9c2857e6b095ab115664b5700e36daafe1cf54f6
Larry Shaffer <lshaffer@boundlessgeo.com>
2019-01-28 01:15:37 -0700

    Nix pip cache and reqs.txt, to avoid run auto-deps in Py3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,11 +13,13 @@ set "GRASS_SITE_PKGS=%LIBRARY_PREFIX%\apps\grass-site-packages"
 if not exist "%GRASS_SITE_PKGS%" mkdir "%GRASS_SITE_PKGS%"
 
 :: Install Python 2 pkgs via pip2
+:: Nix cache, to avoid run auto-dependencies in Py3
 pip2 install ^
   --isolated ^
+  --no-cache-dir ^
   --target="%GRASS_SITE_PKGS%" ^
   --upgrade ^
-  -r %RECIPE_DIR%\requirements.txt
+  -r %RECIPE_DIR%\pip-requirements.txt
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 exit /b 0
